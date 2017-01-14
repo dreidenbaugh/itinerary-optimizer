604ca8aaf1d2e338440bd8b4ba46aca9e8ce9864
Dietrich Reidenbaugh <dreidenbaugh@gwmail.gwu.edu>
2017-01-14 03:04:58 -0700

    Add basic flight price search

--- optimize.js
+++ optimize.js
@@ -1,5 +1,20 @@
 function optimize() {
-    output = form.start.value + "<br>";
+    var startPlace = form.start.value;
+    var endPlace = form.end.value;
+    var startDate = form.startdate.value;
+
+    var xhr = new XMLHttpRequest();
+    var url = "http://partners.api.skyscanner.net/apiservices/browsedates/"
+            + "v1.0/US/USD/en-US/" + startPlace + "/" + endPlace + "/"
+            + startDate + "?apiKey=" + config.API_KEY;
+    xhr.open("GET", url, false)
+    xhr.send();
+    
+    console.log(xhr.status);
+    console.log(xhr.response);
+    var json = JSON.parse(xhr.response);
+    console.log(json);
+    
+    output = "Lowest Price: $" + json.Quotes[0].MinPrice + "<br>";
     document.getElementById("output").innerHTML = output;
-    return false;
 }
