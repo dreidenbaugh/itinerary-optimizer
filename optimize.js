c900211ab21ddababbbfd41169d2921900e88802
Dietrich Reidenbaugh <dreidenbaugh@gwmail.gwu.edu>
2017-01-14 05:00:43 -0700

    Add stops input and searching of all possible routes

--- optimize.js
+++ optimize.js
@@ -1,20 +1,52 @@
 function optimize() {
     // Extract input from form:
     var startPlace = form.start.value;
+    var stopPlaces = form.stops.value.split(",");
     var endPlace = form.end.value;
     var startDate = form.startdate.value;
     
-    // Search for the cheapest flight:
-    var flight = flightSearch(startPlace, endPlace, startDate)
+    // Compile list of all places:
+    if (stopPlaces[0] !== "")
+    {
+        var places = [startPlace].concat(stopPlaces,[endPlace]);
+    }
+    else
+    {
+        var places = [startPlace, endPlace];
+    }
+    console.log(places);
+    
+    // Generate 2D array of cheapest flights for all possible routes
+    var flightsArrays = new Array(places.length - 1);
+    for (var fromIndex = 0; fromIndex < places.length - 1; fromIndex++)
+    {   
+        var flightsRow = new Array(places.length);
+        for (var toIndex = 1; toIndex < places.length; toIndex++)
+        {
+            if (fromIndex == 0 && toIndex == places.length - 1)
+            {
+                break;
+            }
+            if (fromIndex == toIndex)
+            {
+                continue;
+            }
+            flightsRow[toIndex] = flightSearch(places[fromIndex], 
+                    places[toIndex], startDate);
+            console.log(places[fromIndex], places[toIndex], 
+                    flightsRow[toIndex]);
+        }
+        flightsArrays[fromIndex] = flightsRow;
+    }
     
     // Output the results:
-    output = "Lowest Price: $" + flight.price + "<br>";
+    var output = "Complete" + "<br>";
     document.getElementById("output").innerHTML = output;
 }
 
 /**
  * Returns the cheapest flight between originPlace and destinationPlace on the
- * specified date
+ * specified date, or null if a flight could not be found
  * @param {string} originPlace - Place code of the flight origin
  * @param {string} destinationPlace - Place code of the flight destination
  * @param {string} date - Flight date in the format "yyyy-MM-dd"
@@ -34,6 +66,13 @@ function flightSearch(originPlace, destinationPlace, date) {
     var json = JSON.parse(xhr.response);
     console.log(json);
     
-    // Return the flight information:
-    return {price: json.Quotes[0].MinPrice};
+    // Return flight information or null if no flight was found:
+    if (json.Quotes.length > 0)
+    {
+        return {price: json.Quotes[0].MinPrice};
+    }
+    else
+    {
+        return null;
+    }
 }
